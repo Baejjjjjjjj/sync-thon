ba7246eecfe3a00929643c9d04456b672e75c554
Baejjjjjjjj <rew1212@naver.com>
2023-07-04 05:09:14 +0900

    update

--- vote/voteRouter.js
+++ vote/voteRouter.js
@@ -1,12 +1,11 @@
 import express from "express"
 import { jwtMiddleware } from '../config/jwtMiddleware';
-import {GetVotes,postBackEndVote,postFrontVote,postVoteQuestion,postVoteQuestionOption} from "./voteController";
+import {GetVotes,postBackEndVote,postFrontVote} from "./voteController";
 const voteRouter = express.Router();
 
 voteRouter.get('/',jwtMiddleware,GetVotes);
 voteRouter.post('/backend',jwtMiddleware,postBackEndVote);
 voteRouter.post('/front',jwtMiddleware,postFrontVote);
-voteRouter.post('/question',postVoteQuestion);
-voteRouter.post('/question/option',postVoteQuestionOption);
+
 
 export default voteRouter
